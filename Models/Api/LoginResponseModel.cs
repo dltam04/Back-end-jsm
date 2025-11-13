eedbf19772fbaf97c622983519f2a63b78d871b0
dltam04 <zinmrx04@gmail.com>
2025-11-13 10:49:24 +0700

    add Role attribute

--- Models/Api/LoginResponseModel.cs
+++ Models/Api/LoginResponseModel.cs
@@ -2,8 +2,9 @@
 {
     public class LoginResponseModel
     {
-        public string Username { get; set; };
-        public string AccessToken { get; set; };
-        public int ExpiresIn { get; set; };
+        public string Username { get; set; } = string.Empty;
+        public string AccessToken { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+        public int ExpiresIn { get; set; }
     }
 }
