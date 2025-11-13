a461e7efabceaf18beb192ef901f233162aef37f
dltam04 <zinmrx04@gmail.com>
2025-11-13 09:30:30 +0700

    add LoginRequestModel and LoginResponseModel

---
+++ Models/Api/LoginRequestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieApi.Models.Api
+{
+    public class LoginRequestModel
+    {
+        public string Username { get; set; };
+        public string Password { get; set; };
+    }
+}
---
+++ Models/Api/LoginResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieApi.Models.Api
+{
+    public class LoginResponseModel
+    {
+        public string Username { get; set; };
+        public string AccessToken { get; set; };
+        public int ExpiresIn { get; set; };
+    }
+}
