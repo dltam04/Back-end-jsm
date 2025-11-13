36b3d78eca60161265ed32501666724ee3ec5749
dltam04 <zinmrx04@gmail.com>
2025-11-13 10:49:59 +0700

    more class to manage user account

---
+++ Models/UserAccount.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieApi.Models
+{
+    public class UserAccount
+    {
+        public int Id { get; set; }  // Primary Key
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty; // Hashed password
+        public string Role { get; set; } = "User";           // "User" or "Admin"
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
