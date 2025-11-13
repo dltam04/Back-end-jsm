f2ed6e13e188fcd91e96d67af693fb35e4d812ea
dltam04 <zinmrx04@gmail.com>
2025-11-14 00:02:17 +0700

    more in the middel of work...

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MovieApi.Services;
+using MovieApi.Models.Api;
+using Microsoft.AspNetCore.Authorization;
 
 namespace MovieApi.Controllers
 {
@@ -7,5 +10,18 @@ namespace MovieApi.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        private readonly JwtService _jwtService;
+        public AccountController(JwtService jwtService) => _jwtService = jwtService;
+
+        [AllowAnonymous]
+        [HttpPost("Login")]
+        public async Task<ActionResult<LoginResponseModel>> Login(LoginRequestModel request)
+        {
+            var result = await _jwtService.Authenticate(request);
+            if (result is null)
+                return NotFound();
+
+            return result;
+        }
     }
 }
