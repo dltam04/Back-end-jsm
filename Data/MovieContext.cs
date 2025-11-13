9cd3694fd43fa5aedba71a6654ebf9f1a766f599
dltam04 <zinmrx04@gmail.com>
2025-11-13 10:48:28 +0700

    move to Data folder (new) and add UserAccount for later use

---
+++ Data/MovieContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Models;
+
+namespace MovieApi.Data;
+
+public class MovieContext : DbContext
+{
+    public MovieContext(DbContextOptions<MovieContext> options) : base(options) { }
+
+    public DbSet<Movie> Movies => Set<Movie>();
+    public DbSet<UserAccount> UserAccounts { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder b)
+    {
+        base.OnModelCreating(b);
+
+        // map to dbo.Movies explicitly
+        b.Entity<Movie>().ToTable("Movies", "dbo");
+        b.Entity<UserAccount>().ToTable("UserAccounts", "dbo");
+    }
+}
