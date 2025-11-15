a8dd1b2de4c1e36c11e7e675e4ac56c55dc6e245
dltam04 <zinmrx04@gmail.com>
2025-11-15 23:34:28 +0700

    update attribute

--- Models/UserAccount.cs
+++ Models/UserAccount.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }  // Primary Key
         public string UserName { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty; // Hashed password
         public string Role { get; set; } = "User";           // "User" or "Admin"
