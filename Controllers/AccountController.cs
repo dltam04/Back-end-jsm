73b8d4685c533477c7f08029fefb3cef827c39ee
dltam04 <zinmrx04@gmail.com>
2025-11-17 11:17:37 +0700

    add comments

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -6,22 +6,33 @@ using MovieApi.Services;
 
 namespace MovieApi.Controllers
 {
+    // Generate route based on controller name (final route: /api/Account)
     [Route("api/[controller]")]
     [ApiController]
     public class AccountController : ControllerBase
     {
+        /* The controller receives a JwtService instance through constructor injection
+           The service is responsible for:
+           - validate for username and password
+           - verifying user exist in db
+           - generate signed JWT token
+           - return result
+
+          -> Flow: Controller -> JwtService -> DbContext
+        */ 
         private readonly JwtService _jwtService;
         public AccountController(JwtService jwtService) => _jwtService = jwtService;
 
-        [AllowAnonymous]
+        [AllowAnonymous] // Key for authorize (User or Admin can access)
         [HttpPost("Login")]
         public async Task<ActionResult<LoginResponseModel>> Login([FromBody] LoginRequestModel request)
         {
+            // send request to JwtService.Authenticate()
             var result = await _jwtService.Authenticate(request);
             if (result is null)
-                return NotFound();
+                return NotFound(); // return null if credentials are invalid (check from db)
 
-            return Ok(result);
+            return Ok(result); // 200 status
         }
     }
 }
