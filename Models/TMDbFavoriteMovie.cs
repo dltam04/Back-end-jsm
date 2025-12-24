43898c22c1a94a9009c1d909b292598a4276553b
dltam04 <zinmrx04@gmail.com>
2025-12-24 10:27:24 +0700

    tmdb fav class

---
+++ Models/TMDbFavoriteMovie.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieApi.Models
+{
+    public class TMDbFavoriteMovie
+    {
+        public int TMDbAccountId { get; set; }  // TMDb account id
+        public TMDbAccount TMDbAccount { get; set; } = null!;
+
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
