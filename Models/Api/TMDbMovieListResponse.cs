96ad2b4f6dc6d1a03b571083a7797bf20a939edb
dltam04 <zinmrx04@gmail.com>
2025-12-24 10:18:42 +0700

    movies list for homepage

---
+++ Models/Api/TMDbMovieListResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace MovieApi.Models.Api
+{
+    // Response wrapper for /movie/popular, /movie/top_rated, etc.
+    public class TMDbMovieListResponse
+    {
+        [JsonPropertyName("page")]
+        public int Page { get; set; }
+
+        [JsonPropertyName("total_results")]
+        public int TotalResults { get; set; }
+
+        [JsonPropertyName("total_pages")]
+        public int TotalPages { get; set; }
+
+        [JsonPropertyName("results")]
+        public List<TMDbMovieSummary> Results { get; set; } = new();
+    }
+}
