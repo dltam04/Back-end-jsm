151a75f24d9fc46df8149765c0fa8975f5221720
dltam04 <zinmrx04@gmail.com>
2025-12-24 10:22:09 +0700

    genre class

---
+++ Models/Genre.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieApi.Models
+{
+    public class Genre
+    {
+        public int GenreId { get; set; } // TMDb genre id
+        public string Name { get; set; } = null!;
+        public ICollection<MovieGenre> MovieGenres { get; set; } = new List<MovieGenre>();
+    }
+}
