c90ab3f2ee35066a2f23ad95b4048d343d1a2e46
dltam04 <zinmrx04@gmail.com>
2025-12-24 10:25:24 +0700

    handle for popular, top_rated and upcoming

---
+++ Models/MovieListEntry.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieApi.Models
+{
+    public class MovieListEntry
+    {
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; } = null!;
+
+        public string ListType { get; set; } = null!; // "popular", "top_rated", "upcoming"
+        public int Page { get; set; }
+        public int Position { get; set; }
+    }
+}
