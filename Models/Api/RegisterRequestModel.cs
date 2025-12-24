5ae96725cdda73c938cf67a9fc97dde47dba1932
dltam04 <zinmrx04@gmail.com>
2025-12-24 10:12:44 +0700

    submit form for register

---
+++ Models/Api/RegisterRequestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieApi.Models.Api
+{
+    public class RegisterRequestModel
+    {
+        public string FullName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
