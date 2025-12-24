354eed48872eedfb8763886681cf5c0a369bfdd7
dltam04 <zinmrx04@gmail.com>
2025-12-24 10:26:14 +0700

    actor class

---
+++ Models/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace MovieApi.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; } // TMDb person id
+        public string Name { get; set; } = null!;
+        public string? Biography { get; set; }
+        public string? ProfilePath { get; set; }
+        public DateTime? Birthday { get; set; }
+        public string? PlaceOfBirth { get; set; }
+
+        public ICollection<MovieCast> MovieCredits { get; set; } = new List<MovieCast>();
+    }
+}
