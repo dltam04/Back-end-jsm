6e168314c9072f24767d3899d00090d60f4a397b
dltam04 <zinmrx04@gmail.com>
2025-12-24 10:29:38 +0700

    email verification logic handle

---
+++ Services/EmailService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace MovieApi.Services
+{
+    public class EmailService
+    {
+        private readonly IConfiguration _config;
+
+        public EmailService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string htmlBody)
+        {
+            var host = _config["Smtp:Host"];
+            var port = int.Parse(_config["Smtp:Port"]!);
+            var username = _config["Smtp:Username"];
+            var appPassword = _config["Smtp:AppPassword"];
+            var fromEmail = _config["Smtp:FromEmail"];
+            var fromName = _config["Smtp:FromName"];
+
+            using var client = new SmtpClient(host!, port)
+            {
+                EnableSsl = true,
+                Credentials = new NetworkCredential(username, appPassword)
+            };
+
+            using var message = new MailMessage
+            {
+                From = new MailAddress(fromEmail!, fromName),
+                Subject = subject,
+                Body = htmlBody,
+                IsBodyHtml = true
+            };
+
+            message.To.Add(toEmail);
+
+            await client.SendMailAsync(message);
+        }
+    }
+}
