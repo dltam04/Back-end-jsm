fd92b8e48a8a9b794a28d128988eeaf66e2e7a06
dltam04 <zinmrx04@gmail.com>
2025-12-24 10:27:04 +0700

    tmdb account class

---
+++ Models/TMDbAccount.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieApi.Models
+{
+    public class TMDbAccount
+    {
+        public int TMDbAccountId { get; set; }         // TMDb account id
+        public string Username { get; set; } = null!;
+        public string SessionId { get; set; } = null!; // TMDb session_id (if you want)
+
+        public ICollection<TMDbFavoriteMovie> Favorites { get; set; } = new List<TMDbFavoriteMovie>();
+        public ICollection<TMDbWatchlistMovie> Watchlist { get; set; } = new List<TMDbWatchlistMovie>();
+    }
+}
