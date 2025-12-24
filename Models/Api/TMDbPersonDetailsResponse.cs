f2ea38e59504e9c2db5418d15fbed39799a31a3f
dltam04 <zinmrx04@gmail.com>
2025-12-24 10:20:13 +0700

    actor detail data

---
+++ Models/Api/TMDbPersonDetailsResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MovieApi.Models.Api
+{
+    public class TMDbPersonDetailsResponse
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Biography { get; set; }
+
+        [JsonPropertyName("profile_path")]
+        public string? ProfilePath { get; set; }
+        public string? Birthday { get; set; }
+
+        [JsonPropertyName("place_of_birth")]
+        public string? PlaceOfBirth { get; set; }
+    }
+}
