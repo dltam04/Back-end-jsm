4d16ba521639c4dde5ebf23b975c9f0b206c00f7
dltam04 <zinmrx04@gmail.com>
2025-12-24 10:14:28 +0700

    reset password requirement

---
+++ Models/Api/ResetPasswordRequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieApi.Models.Api
+{
+    public class ResetPasswordRequestModel
+    {
+        public string Token { get; set; } = null!;
+        public string NewPassword { get; set; } = null!;
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
