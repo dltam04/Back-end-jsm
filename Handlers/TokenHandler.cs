6d7ca86d7e9fb3720c8103d37eecdab54b4cb3a9
dltam04 <zinmrx04@gmail.com>
2025-12-24 10:11:21 +0700

    email verification token handler

---
+++ Handlers/TokenHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace MovieApi.Handlers
+{
+    public static class TokenHandler
+    {
+        public static string GenerateToken()
+        {
+            var bytes = RandomNumberGenerator.GetBytes(32);
+            return Convert.ToBase64String(bytes)
+                .Replace("+", "-").Replace("/", "_").Replace("=", "");
+        }
+
+        public static string Sha256(string input)
+        {
+            using var sha = SHA256.Create();
+            var bytes = sha.ComputeHash(Encoding.UTF8.GetBytes(input));
+            return Convert.ToHexString(bytes);
+        }
+    }
+}
