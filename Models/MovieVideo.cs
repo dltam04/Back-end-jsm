daac6791b1d0ae3436c70b85e013b04428235548
dltam04 <zinmrx04@gmail.com>
2025-12-24 10:25:51 +0700

    movie related video

---
+++ Models/MovieVideo.cs
@@ -0,0 +1,14 @@
+﻿namespace MovieApi.Models
+{
+    public class MovieVideo
+    {
+        public int MovieVideoId { get; set; }
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; } = null!;
+
+        public string Key { get; set; } = null!;
+        public string Site { get; set; } = null!;
+        public string Type { get; set; } = null!;
+        public string Name { get; set; } = null!;
+    }
+}
