b66a221fdc9cfed93b033f9923e03a9644610964
dltam04 <zinmrx04@gmail.com>
2025-12-24 10:28:04 +0700

    personal account class

--- Models/UserAccount.cs
+++ Models/UserAccount.cs
@@ -9,5 +9,20 @@
         public string Password { get; set; } = string.Empty; // Hashed password
         public string Role { get; set; } = "User";           // "User" or "Admin"
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Email verification part
+        public bool IsEmailVerified { get; set; } = false;
+        public string? EmailVerificationTokenHash { get; set; }
+        public DateTime? EmailVerificationTokenExpiresAt { get; set; }
+        public DateTime? EmailVerifiedAt { get; set; }
+
+        // Password reset part
+        public string? PasswordResetTokenHash { get; set; }
+        public DateTime? PasswordResetTokenExpiresAt { get; set; }
+        public DateTime? PasswordResetRequestedAt { get; set; }
+
+
+        public ICollection<UserFavoriteMovie> FavoriteMovies { get; set; } = new List<UserFavoriteMovie>();
+        public ICollection<UserWatchlistMovie> WatchlistMovies { get; set; } = new List<UserWatchlistMovie>();
     }
 }
