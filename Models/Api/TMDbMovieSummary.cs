fe79321af427c5bc4dd570b1fb4005eb1a9a4b39
dltam04 <zinmrx04@gmail.com>
2025-12-24 10:19:39 +0700

    movie detail data to view on frontend

---
+++ Models/Api/TMDbMovieSummary.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace MovieApi.Models.Api
+{
+    // Basic movie info coming from TMDb "list" endpoints
+    public class TMDbMovieSummary
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("title")]
+        public string? Title { get; set; }
+
+        [JsonPropertyName("original_title")]
+        public string? OriginalTitle { get; set; }
+
+        [JsonPropertyName("overview")]
+        public string? Overview { get; set; }
+
+        [JsonPropertyName("poster_path")]
+        public string? PosterPath { get; set; }
+
+        [JsonPropertyName("backdrop_path")]
+        public string? BackdropPath { get; set; }
+
+        [JsonPropertyName("release_date")]
+        public string? ReleaseDate { get; set; }
+
+        [JsonPropertyName("popularity")]
+        public double Popularity { get; set; }
+
+        [JsonPropertyName("vote_average")]
+        public double VoteAverage { get; set; }
+
+        [JsonPropertyName("vote_count")]
+        public int VoteCount { get; set; }
+
+        [JsonPropertyName("original_language")]
+        public string? OriginalLanguage { get; set; }
+
+        // list of genre IDs from TMDb
+        [JsonPropertyName("genre_ids")]
+        public List<int>? GenreIds { get; set; }
+    }
+}
