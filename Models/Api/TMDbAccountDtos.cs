acfaf722ab4cd95c28d2c91ab5802a3d7571665f
dltam04 <zinmrx04@gmail.com>
2025-12-24 10:15:52 +0700

    toggle when new TMDb account appear in the database

---
+++ Models/Api/TMDbAccountDtos.cs
@@ -0,0 +1,23 @@
+﻿namespace MovieApi.Models.Api
+{
+    public class TMDbAccountDto
+    {
+        public int TMDbAccountId { get; set; }             // TMDb account id
+        public string Username { get; set; } = null!;
+
+        // movie ids
+        public List<int> Favorites { get; set; } = new();
+        public List<int> Watchlist { get; set; } = new();
+    }
+
+    // What ACCEPT on POST/PUT (when you create/update from TMDb login flow)
+    public class TMDbAccountCreateUpdateDto
+    {
+        public int TMDbAccountId { get; set; }             // TMDb account id
+        public string Username { get; set; } = null!;
+        public string SessionId { get; set; } = null!;
+
+        public List<int>? Favorites { get; set; }
+        public List<int>? Watchlist { get; set; }
+    }
+}
