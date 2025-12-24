acbdddb3e473547df98f648d08f5efcebfd0e22f
dltam04 <zinmrx04@gmail.com>
2025-12-24 10:24:23 +0700

    movie link

---
+++ Models/MovieLink.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieApi.Models
+{
+    [Table("links")]
+    public class MovieLink
+    {
+        [Key]
+        [Column("movieId")]
+        public int MovieId { get; set; }      // MovieLens id
+
+        [Column("imdbId")]
+        public int? ImdbId { get; set; }
+
+        [Column("tmdbId")]
+        public int? TmdbId { get; set; }
+    }
+}
