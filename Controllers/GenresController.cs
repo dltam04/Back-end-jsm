00c53f222b2599f66502ba637b8cf0d8ac77fa4a
dltam04 <zinmrx04@gmail.com>
2025-12-24 09:49:42 +0700

    genres api

---
+++ Controllers/GenresController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using MovieApi.Data;
+
+namespace MovieApi.Controllers
+{
+    [ApiController]
+    [Route("api/genres")]
+    [AllowAnonymous]
+    public class GenresController : ControllerBase
+    {
+        private readonly MovieContext _context;
+
+        public GenresController(MovieContext context)
+        {
+            _context = context;
+        }
+
+        // GET /api/genres
+        [HttpGet]
+        public async Task<ActionResult<object>> GetGenres()
+        {
+            var genres = await _context.Genres
+                .AsNoTracking()
+                .Select(g => new
+                {
+                    id = g.GenreId,
+                    name = g.Name
+                })
+                .ToListAsync();
+
+            return Ok(new { genres });
+        }
+    }
+}
