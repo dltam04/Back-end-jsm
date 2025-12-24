b4588eaccfec91a91591097a32e3542ebb89c1ef
dltam04 <zinmrx04@gmail.com>
2025-12-24 10:28:37 +0700

    personal account fav list class

---
+++ Models/UserFavoriteMovie.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieApi.Models
+{
+    public class UserFavoriteMovie
+    {
+        public int UserAccountId { get; set; } // UserId
+        public UserAccount User { get; set; } = null!;
+
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
