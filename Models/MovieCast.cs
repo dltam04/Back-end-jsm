c0100ed3534cdb3bc34d9b8f24c3da0cc7d4eb84
dltam04 <zinmrx04@gmail.com>
2025-12-24 10:23:29 +0700

    actor per movie class

---
+++ Models/MovieCast.cs
@@ -0,0 +1,14 @@
+﻿namespace MovieApi.Models
+{
+    public class MovieCast
+    {
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; } = null!;
+
+        public int PersonId { get; set; }
+        public Person Person { get; set; } = null!;
+
+        public string? Character { get; set; }
+        public int CastOrder { get; set; }
+    }
+}
