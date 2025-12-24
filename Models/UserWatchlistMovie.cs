d8f887a93fba955630a9f181753869caf6a802c8
dltam04 <zinmrx04@gmail.com>
2025-12-24 10:29:00 +0700

    personal account watchlist class

---
+++ Models/UserWatchlistMovie.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieApi.Models
+{
+    public class UserWatchlistMovie
+    {
+        public int UserAccountId { get; set; }  // FK to UserAccounts.Id
+        public UserAccount User { get; set; } = null!;
+
+        public int MovieId { get; set; }        // FK to Movies.MovieId
+        public Movie Movie { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
